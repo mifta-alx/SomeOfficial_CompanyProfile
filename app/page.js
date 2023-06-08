f372436bf4a75d18b1e93f33fad9ffa8dcd8dd12
alx-cpu <alx.yoi67@gmail.com>
2023-06-08 12:47:31 +0700

    Update UI

--- app/page.js
+++ app/page.js
@@ -2,6 +2,11 @@
 import Image from 'next/image'
 import { useState } from 'react'
 import { Plus_Jakarta_Sans } from 'next/font/google'
+import localFont from 'next/font/local';
+import { Icon } from '@iconify/react';
+import Head from 'next/head'
+
+const myFont = localFont({ src: './HappyTimeThree.otf' });
 
 const pjs = Plus_Jakarta_Sans({
   subsets: ['latin'],
@@ -11,6 +16,21 @@ const pjs = Plus_Jakarta_Sans({
 
 export default function Home() {
   const [toggle, setToggle] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const dataImage = [
+    {
+      'url': '/bucket.png'
+    },
+    {
+      'url': '/mug.png'
+    },
+    {
+      'url': '/bucket.png'
+    },
+    {
+      'url': '/bracellet.png'
+    },
+  ]
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-white">
       <nav className="bg-transparent w-full z-20 top-0 left-0 absolute">
@@ -42,137 +62,90 @@ export default function Home() {
           </div>
         </div>
       </nav>
-
-      <img src="/banner.jpg" alt="" />
-      <section id='home' className='text-black px-[90px]'>
-        <div className='py-[60px]'>
-          <h1 className={`text-4xl text-center mb-5 font-bold ${pjs.className}`}>Our Background</h1>
+      <img src="/banner.jpg" alt="" className='md:h-full h-screen object-cover object-center w-full' />
+      <section id='home' className='text-[#2E2E2e] md:px-[90px] px-10'>
+        <div className=' md:py-[60px] pt-12'>
+          <h1 className={`md:text-4xl text-3xl text-center mb-5 font-bold ${pjs.className}`}>Our Background</h1>
           <p className={`text-center font-medium text-sm ${pjs.className}`}>SOME adalah brand apparel yang menghadirkan keindahan mitologi dunia melalui desain-desain kreatif. Dalam setiap koleksi kami, kami menampilkan dewa-dewa dari berbagai budaya dengan menyoroti sisi baik dan buruk mereka. Melalui penggabungan estetika modern dengan simbolisme kuno, kami menciptakan produk berkualitas tinggi yang menggambarkan kekuatan dan keindahan dewa-dewa. Temukan cerita yang kuat di balik setiap produk SOME yang Anda miliki, dan bergabunglah dalam menjelajahi dunia yang menakjubkan ini bersama kami.</p>
         </div>
       </section>
-      <div className='px-[90px] py-10'>
-        <div className='flex flex-row justify-between gap-24 items-center'>
+      <div className='md:px-[90px] md:py-10'>
+        <div className='flex md:flex-row flex-col justify-between md:gap-24 items-center'>
           <div>
-            <img src="/02.png" alt="" className='max-w-xl' />
+            <img src="/02.png" alt="" className='md:max-w-xl max-w-xs' />
           </div>
-          <div className='text-black'>
-            <h1 className={`text-[40px] font-bold ${pjs.className} mb-[30px] max-w-sm`}>Eco Friendly Packaging</h1>
-            <p className={`text-base leading-6 ${pjs.className} font-light`}>Kami menggunakan bahan-bahan yang bertanggung jawab secara lingkungan, kami memastikan bahwa kemasan kami memiliki dampak minimal terhadap planet kita. Kami menggunakan bahan daur ulang atau dapat terurai untuk kotak dan kemasan kami. Dengan berkomitmen untuk eco-friendly packaging, kami berharap dapat memberikan kontribusi positif terhadap perlindungan lingkungan dan menginspirasi pelanggan kami untuk mengadopsi praktek yang serupa.</p>
+          <div className='text-[#2E2E2e] px-10'>
+            <h1 className={`md:text-[40px] text-3xl font-bold ${pjs.className} mb-[30px] md:max-w-sm md:text-left text-center`}>Eco Friendly Packaging</h1>
+            <p className={`md:text-base text-sm leading-6 ${pjs.className} font-light md:text-left text-center`}>Kami menggunakan bahan-bahan yang bertanggung jawab secara lingkungan, kami memastikan bahwa kemasan kami memiliki dampak minimal terhadap planet kita. Kami menggunakan bahan daur ulang atau dapat terurai untuk kotak dan kemasan kami. Dengan berkomitmen untuk eco-friendly packaging, kami berharap dapat memberikan kontribusi positif terhadap perlindungan lingkungan dan menginspirasi pelanggan kami untuk mengadopsi praktek yang serupa.</p>
           </div>
         </div>
       </div>
-      <div className='bg-[#FAFAFA] text-black py-[54px] w-full px-[90px]'>
-        <div className={`flex flex-row gap-12 items-center`}>
+      <div className='bg-[#FAFAFA] text-[#2E2E2e] py-[54px] w-full px-[90px]'>
+        <div className={`flex flex-row gap-12 items-center mb-[50px]`}>
           <h1 className={`text-5xl text-left mb-5 font-bold ${pjs.className} max-w-md`}>Bringing You the Elements of Style</h1>
-          <p>Circle back minimize backwards overflow yet product need full resourcing and support from a cross-functional team in order to be built, maintained, and evolved.</p>
+          <p className={`${pjs.className} font-normal`}>Circle back minimize backwards overflow yet product need full resourcing and support from a cross-functional team in order to be built, maintained, and evolved.</p>
         </div>
-          <div className='h-1 flex my-[50px] w-full'/>
-      </div>
-      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className='grid grid-cols-2 gap-10 pt-[50px] border-t border-[#2E2E2E]'>
+          <img src="/duo_shirt.jpg" alt="" className='max-h-56 w-full object-cover' />
+          <img src="/women_shirt.jpg" alt="" className='max-h-56 w-full object-cover' />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='py-[10px] w-full'>
+        <img src="/window.png" alt="" className='w-full object-cover' />
+        <div className='py-[10px] mt-10 px-[90px]'>
+          <p className={`${pjs.className} font-medium text-[#2E2E2E]`}>SOME adalah sebuah brand yang menggabungkan seni dan mitologi dalam desain kami. Kami percaya bahwa cerita dan keajaiban mitologi dapat diabadikan dalam karya seni yang dipakai setiap hari. Setiap artikel kami menciptakan pengalaman unik dengan menggambarkan dewa-dewa dari berbagai mitologi, memadukan simbolisme dan keindahan dalam setiap detailnya. Selain menciptakan pakaian yang menginspirasi, kami juga mengutamakan keberlanjutan dengan menggunakan bahan ramah lingkungan dan proses produksi yang bertanggung jawab. Dengan merek SOME, kami mengajak Anda untuk merayakan keajaiban mitologi melalui mode yang unik dan berdaya tahan, mengingatkan kita akan warisan budaya dan keindahan yang ada di sekitar kita.</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
+      <div className='flex md:py-10 md:px-[90px] flex-col gap-[30px] w-full'>
+        <div className='flex flex-row justify-between items-center'>
+          <h1 className={`${pjs.className} font-bold text-4xl text-black`}>Our Products</h1>
+          <div className='flex flex-row items-center gap-6'>
+            <button type="button" className='text-black border border-black p-3' onClick={() => setCurrentIndex(0)}><Icon icon="formkit:arrowleft" className='w-5' /></button>
+            <button type="button" className='text-white flex flex-row items-center px-4 py-2 border border-black bg-black' onClick={() => setCurrentIndex(currentIndex + 2)}>Next<Icon icon="formkit:arrowright" className='ml-2 w-5' /></button>
+          </div>
+        </div>
+        <div className='flex flex-row gap-8 border-t border-[#2E2E2E] pt-[30px]'>
+          {
+            dataImage.slice(currentIndex, currentIndex + 2).map((data) => {
+              return (
+                <img src={data.url} alt="" className='h-96 w-full object-cover' />
+              )
+            })
+          }
+        </div>
+      </div>
+      <footer className='bg-[#2E2E2E] py-10 px-[90px] w-full flex flex-col gap-10'>
+        <img src="/logo.png" className="h-9 w-fit" alt="Logo" />
+        <div className='max-w-2xl'>
+          <h1 className='text-5xl'>Rewrite the <span className={`${myFont.className}`}>Myth</span> and repair the legend of the <span className={`${myFont.className}`}>Gods.</span></h1>
+        </div>
+        <div className='border-t border-[#696969] pt-10'>
+          <div className='flex flex-col gap-5'>
+            <div className='flex flex-row justify-between items-center'>
+              <ul className={`flex flex-row gap-4 text-white ${pjs.className} font-normal`}>
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+              <ul className={`flex flex-row gap-4 text-white ${pjs.className} font-normal`}>
+                <li><a href="https://www.instagram.com/someofficial.id/"><Icon icon="mdi:instagram" /></a></li>
+                <li><a href="https://www.tiktok.com/@someofficial.id?is_from_webapp=1&sender_device=pc"><Icon icon="ic:outline-tiktok" /></a></li>
+                <li><Icon icon="uil:facebook" /></li>
+              </ul>
+            </div>
+            <p className={`text-xs ${pjs.className} text-center font-medium`}>Some Official | Copyright Ⓒ 2023 | All Rights reserved</p>
+          </div>
+        </div>
+      </footer>
     </main>
   )
 }
